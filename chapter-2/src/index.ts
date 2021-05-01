5a54005bf26e2d31ae3244535a04046433be4b86
MasatoShima <masato.shima@icloud.com>
2021-05-02 03:18:37 +0900

    Initial commit.

---
+++ chapter-2/src/index.ts
@@ -0,0 +1,25 @@
+// """
+// Name: index.ts
+// Created by: Masato Shima
+// Created on: 2021/05/02
+// Description: index.ts
+// """
+
+console.log("Hello TypeScript !")
+
+let a = 1 + 2
+console.log(a)
+
+let b = a + 3
+console.log(b)
+
+let c = {
+    apple: a,
+    banana: b
+}
+console.log(c)
+
+let d = c.apple * 5
+console.log(d)
+
+// End
